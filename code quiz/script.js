3d5624b9098841c3b11be4fc8ba109622b89b049
Justin Rhee <jayrhee0514@gmail.com>
2020-10-31 19:04:40 -0400

    able to add user to click answers

--- code quiz/script.js
+++ code quiz/script.js
@@ -26,3 +26,19 @@ function goToNextQuestion(userClick) {
 
 
 }
+//setting up a way to click the correct answer
+function answerClickSetUp() {
+    var a = document.getElementById("A");
+    var b = document.getElementById("B");
+    var c = document.getElementById("C");
+    var d = document.getElementById("D");
+  
+    a.addEventListener("click", function () { goToNextQuestion(a.innerText); });
+    b.addEventListener("click", function () { goToNextQuestion(b.innerText); });
+    c.addEventListener("click", function () { goToNextQuestion(c.innerText); });
+    d.addEventListener("click", function () { goToNextQuestion(d.innerText); });
+  }
+  answerClickSetUp();
+  
+ 
+  
