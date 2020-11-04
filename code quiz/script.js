7f2c0b8dde8815c5b4f8b43fa170ff54d84dd5f8
Justin Rhee <jayrhee0514@gmail.com>
2020-11-03 20:06:51 -0500

    got some tip from my tutor on how to do local storage still needs some work

--- code quiz/script.js
+++ code quiz/script.js
@@ -28,17 +28,62 @@ function goToNextQuestion(userClick) {
 }
 //setting up a way to click the correct answer
 function answerClickSetUp() {
-    var a = document.getElementById("A");
-    var b = document.getElementById("B");
-    var c = document.getElementById("C");
-    var d = document.getElementById("D");
+  var a = document.getElementById("A");
+  var b = document.getElementById("B");
+  var c = document.getElementById("C");
+  var d = document.getElementById("D");
+
+  a.addEventListener("click", function () { goToNextQuestion(a.innerText); });
+  b.addEventListener("click", function () { goToNextQuestion(b.innerText); });
+  c.addEventListener("click", function () { goToNextQuestion(c.innerText); });
+  d.addEventListener("click", function () { goToNextQuestion(d.innerText); });
+}
+answerClickSetUp();
+
+startQuiz.addEventListener("click", function () {
+  getNewQuestion(currentQuestionIndex);
+});
+//after question was answered it will change to the next
+var currentQuestion;
+function getNewQuestion(questionIndex) {
+  var question = questions[questionIndex];
+  currentQuestion = question;
+  var title = question.title;
+
+  var questionEl = document.getElementById("question");
+  questionEl.innerText = title;
+
+  var option1 = question.choices[0];
+  var answerEl1 = document.getElementById("A");
+  answerEl1.innerText = option1;
   
-    a.addEventListener("click", function () { goToNextQuestion(a.innerText); });
-    b.addEventListener("click", function () { goToNextQuestion(b.innerText); });
-    c.addEventListener("click", function () { goToNextQuestion(c.innerText); });
-    d.addEventListener("click", function () { goToNextQuestion(d.innerText); });
-  }
-  answerClickSetUp();
-  
+  var option2 = question.choices[1];
+  var answerEl2 = document.getElementById("B");
+  answerEl2.innerText = option2;
  
+
+  var option3 = question.choices[2];
+  var answerEl3 = document.getElementById("C");
+  answerEl3.innerText = option3;
   
+
+  var option4 = question.choices[3];
+  var answerEl4 = document.getElementById("D");
+  answerEl4.innerText = option4;
+ 
+  document.getElementById("question").innerText = title;
+  document.getElementById("A").innerText = choice1;
+  document.getElementById("B").innerText = choice2;
+  document.getElementById("C").innerText = choice3;
+  document.getElementById("D").innerText = choice4;
+
+
+}
+// //adding scores
+// function scoreKeeper(){
+//   document.getElementById("score").innerHTML = count++;
+
+// }
+// scoreKeeper();
+localStorage.setItem("score", 0)
+localStorage.getItem("score")
